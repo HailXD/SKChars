42eca58be474a9d8f6231d8ea7e1f51b3b247a6e
Hail <jyealc@gmail.com>
2025-07-19 11:29:47 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import re
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import pickle as pkl
+
+# website = requests.get('https://soul-knight.fandom.com/wiki/Characters').text
+# with open('characters.html', 'w', encoding='utf-8') as f:
+#     f.write(website)
+
+base = "https://soul-knight.fandom.com"
+tables = pd.read_html("characters.html", extract_links="all")
+df = tables[1]
+
+def _pretty(col):
+    if isinstance(col, tuple):
+        if col[0]:
+            return col[0]
+        if col[1]:
+            return col[1].split("/")[-1].replace("_", " ").title()
+    return str(col)
+
+df.columns = [_pretty(c) for c in df.columns]
+df = df[['Character', 'ID']]
+df['URL'] = df['Character'].apply(lambda x: x[1] if isinstance(x, tuple) else None)
+df["Character"] = df["Character"].apply(lambda x: x[0] if isinstance(x, tuple) else x)
+df['ID'] = [_pretty(c).lstrip('0') for c in df['ID']]
+df['ID'] = df['ID'].replace('', '0')
+df['ID'] = df['ID'].astype(int)
+df = df[df['ID'] < 100]
+
+for row in df.iterrows():
+    print(row)
+    name = row[["Character"]]
+    url = base + row["URL"]
+    
+    pd.read_html(url)
+    with open(f"characters/{name}.pkl", "wb") as f:
+        pkl.dump(row, f)
+    break
